01e3ecb50a05130ac9c54d475c2beebebd5dfa15
Rohit Kumar Pandey <rohitpandey@Rohits-MacBook-Air.local>
2024-07-14 20:22:20 +0530

    Second commit just added some documentation

--- add.go
+++ add.go
@@ -1,5 +1,9 @@
+//This package takes two int number and returns sum of them
+
 package twointsum
 
+// Add takes two int as parameter and return an int which is sum of both the input parameter same as [numberadditon]
+// [numberaddition]:  https://www.mathsisfun.com/numbers/addition.html
 func Add(a, b int) int {
 	return a + b
 }
